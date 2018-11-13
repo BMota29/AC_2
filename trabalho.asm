136fa108483a53d2e090a7f9f2720531e0abc83a
Bruno Mota <a44586@alunos.isel.pt>
2018-11-13 22:39:04 +0000

    ascToUintArr try

--- trabalho.asm
+++ trabalho.asm
@@ -4,14 +4,14 @@
 	.data
 multiply10_aux:
 	.space 2
-multiply10_r1: 
+multiply10_r1:
 	.space 2
-multiply10_r2: 
-	.space 2	
-val:	
+multiply10_r2:
+	.space 2
+val:
 	.word	9
-	
-	
+
+
 ascToUint_r1:
 	.space 2
 ascToUint_r2:
@@ -24,7 +24,7 @@ ascToUint_aux:
 	.space 2
 r5_backup:
 	.space 2
-	
+
 	.equ DATA_DIM, 5
 data:
 	.asciiz "12345"
@@ -41,18 +41,18 @@ main:
 	uint16 multiply10( uint16 val ){
 		return (val << 3) + (val << 1);
 	}
-*/	
+*/
 multiply10:
-	st r1, multiply10_r1 	
-	st r2, multiply10_r2	
+	st r1, multiply10_r1
+	st r2, multiply10_r2
 	shl r1, r0, #3, 0		; (val << 3)
 	shl r2, r0, #1, 0		; (val << 1)
 	add r0, r1, r2			; return r1 + r2
-	ld r1, multiply10_r1	
-	ld r2, multiply10_r2	
+	ld r1, multiply10_r1
+	ld r2, multiply10_r2
 	ret
-	
-	
+
+
 /*	ex: 4.2
 	uint16 ascToUint( uint8 data[] ){
 		uint16 res = 0;
@@ -61,7 +61,7 @@ multiply10:
 		}
 		return res;
 	}
-*/	
+*/
 
 ascToUint:				;total => 126clk => 252ms
 	st r1, ascToUint_r1		;6clk => 12ms
@@ -71,9 +71,9 @@ ascToUint:				;total => 126clk => 252ms
 	add  r3, r0, #0			;4clk => 8ms
 	ldi r0, #0			;6clk => 12ms
 	ldi r1, #0			;6clk => 12ms		;int i = 0
-	ldi r2, #0xF			;6clk => 12ms	
+	ldi r2, #0xF			;6clk => 12ms
 	ldi r4, #'0'			;6clk => 12ms		;store 30 on r4
-	
+
 for:
 	ldb r2, [r3, r1]		;6clk => 12ms		;data[i]
 	sub r2, r2, #0			;4clk => 8ms		;data[i] != 0
@@ -92,31 +92,31 @@ end_for:
 	ld r3, ascToUint_r3		;6clk => 12ms
 	ld r4, ascToUint_r4		;6clk => 12ms
 	ret				;4clk => 8ms
-	
+
 	/*
 	Resposta 4.2:
 		724clk => 1448ms
 	*/
-	
-/*	
-	#define TMP_SIZE 10 
-	uint8 tmp[TMP_SIZE]; 
- 
-	uint16 ascToUintArr( uint16 dst[], uint16 dlen, uint8 src[] ){  
-		uint16 di = 0, si = 0; 
-		while( di < dlen ){   
-			while( src[si] < '0' || src[si] > '9'){    
-				if( src[si++] == 0 ){     
-					return di;    
-				}   
-			}   
-			uint16 ti = 0;   
-			while( src[si] >= '0' && src[si] <= '9' ){    
-				tmp[ ti++ ] = src[ si++ ];   
-			}   tmp[ ti++ ] = 0;   
-			dst[ di++ ] = ascToUint( tmp );  
-		}  
-		return di; 
+
+/*
+	#define TMP_SIZE 10
+	uint8 tmp[TMP_SIZE];
+
+	uint16 ascToUintArr( uint16 dst[], uint16 dlen, uint8 src[] ){
+		uint16 di = 0, si = 0;
+		while( di < dlen ){
+			while( src[si] < '0' || src[si] > '9'){
+				if( src[si++] == 0 ){
+					return di;
+				}
+			}
+			uint16 ti = 0;
+			while( src[si] >= '0' && src[si] <= '9' ){
+				tmp[ ti++ ] = src[ si++ ];
+			}   tmp[ ti++ ] = 0;
+			dst[ di++ ] = ascToUint( tmp );
+		}
+		return di;
 	}
 */
 
@@ -124,8 +124,25 @@ end_for:
 src:
 	.word 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 
+/*
+	dst[] = r0
+	dlen  = r1
+	scr[] = r2
+*/
+ascToUintArr:
+	st r3, ascToUintArr_r3
+	st r4, ascToUintArr_r4
+	ldi r3, #0								;di = 0
+	ldi r4, #0								;si = 0
 
-		
-	
-		
+while1:
+	subr r3, r3, r1						;di < dlen
+	jc end_while1
+
+while2:
+	ldi r?, [r2,r4]						;src[si] < '0'
+	subr r?, r?, #'0'						;...
+end_while1:
+
+
 .end
